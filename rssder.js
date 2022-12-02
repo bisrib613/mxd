eab44793e6aff51e5cec186260936414593c68de
bisrib613 <89772807+bisrib613@users.noreply.github.com>
2022-12-03 00:48:09 +0700

    Create rssder.js

---
+++ rssder.js
@@ -0,0 +1,44 @@
+var url_string = document.URL;
+const params = new URL(url_string).searchParams;
+const c = params.get('book'); // "1"
+var name = params.get('name'); // "bima"
+
+switch (name) {
+    case 'aku':
+        var LP = "https://forsharedpdf.site/"
+        break;
+    case 'ipik':
+        var LP = "https://kindleebs.xyz/"
+        break;
+    case 'eka':
+        var LP = "https://read.forsharedpdf.site/"
+        break;
+    case 'cv':
+        var LP = "https://cv.kindledeals.club/"
+        break;
+    case 'bima':
+        var LP = "https://kindledeals.club/"
+        break;
+    case 'anas':
+        var LP = "https://best.kindledeals.club/"
+        break;
+   
+}
+
+
+if(c!=null){
+
+setTimeout('Redirect()', 1000)
+           }else{
+    window.setTimeout(function(){
+
+        
+       document.getElementById('redir').href=LP;
+
+    }, 500);
+           }
+
+function Redirect() 
+{ 
+window.location.replace(LP+'?book='+c);
+}
